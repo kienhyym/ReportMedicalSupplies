5d08d2a48bc2983f8d9cfa56fb6930170e83c47e
kiennd <kiennd@healthchain.vn>
2020-05-31 08:21:48 +0700

    update

--- application/controllers/donvi_api.py
+++ application/controllers/donvi_api.py
@@ -833,13 +833,12 @@ async def get_thongke_tinhthanh_boyte(tinhthanh_id, tuyendonvi_id, medical_suppl
                 reportOrganizatiobegin_net_amountfromDayToDay = db.session.query(ReportOrganizationDetail).filter(and_(ReportOrganizationDetail.organization_id == idOrganizations,ReportOrganizationDetail.medical_supplies_id == medical_supplies_id,ReportOrganizationDetail.date >= start_time,ReportOrganizationDetail.date <= end_time)).order_by(ReportOrganizationDetail.date.asc()).first()
                 if reportOrganizatiobegin_net_amountfromDayToDay is not None:
                     begin_net_amount = to_dict(reportOrganizatiobegin_net_amountfromDayToDay)['begin_net_amount'] + begin_net_amount       
-            
-            # TỒN TRƯỚC ĐO
+            # TỒN TRƯỚC TRƯỚC KHOẢNG THỜI GIAN
             reportOrganizationDetailBefore = db.session.query(func.sum(ReportOrganizationDetail.quantity_import)-func.sum(ReportOrganizationDetail.quantity_export)).group_by(ReportOrganizationDetail.medical_supplies_id).filter(and_(ReportOrganizationDetail.organization_id.in_(listIDorganizations),ReportOrganizationDetail.medical_supplies_id == medical_supplies_id,ReportOrganizationDetail.date < start_time)).all()
             if len(reportOrganizationDetailBefore) > 0:
                 begin_net_amount = reportOrganizationDetailBefore[0][0] + begin_net_amount
             for idOrganizationsBefore in listIDorganizations:
-                reportOrganizatiobegin_net_amountBefore = db.session.query(ReportOrganizationDetail).filter(and_(ReportOrganizationDetail.organization_id == idOrganizationsBefore,ReportOrganizationDetail.medical_supplies_id == medical_supplies_id,ReportOrganizationDetail.date <= start_time)).order_by(ReportOrganizationDetail.date.asc()).first()
+                reportOrganizatiobegin_net_amountBefore = db.session.query(ReportOrganizationDetail).filter(and_(ReportOrganizationDetail.organization_id == idOrganizationsBefore,ReportOrganizationDetail.medical_supplies_id == medical_supplies_id,ReportOrganizationDetail.date < start_time)).order_by(ReportOrganizationDetail.date.asc()).first()
                 if reportOrganizatiobegin_net_amountBefore is not None:
                     begin_net_amount = to_dict(reportOrganizatiobegin_net_amountBefore)['begin_net_amount'] + begin_net_amount 
 
