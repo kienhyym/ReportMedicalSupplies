f0d73e413a47102d55fe7c000dbbd940d5455506
kiennd <kiennd@healthchain.vn>
2020-06-25 11:01:41 +0700

    update

--- application/controllers/quanlykho_api.py
+++ application/controllers/quanlykho_api.py
@@ -216,11 +216,11 @@ async def load_organization_dropdown_hospital(request):
 async def load_organization_dropdown_other(request):
     item_delete = db.session.query(TuyenDonVi).filter(TuyenDonVi.id == "10").first()
     item_delete2 = db.session.query(Organization).filter(Organization.tuyendonvi_id == "10").first()
-    item_delete3 = db.session.query(ReportOrganizationDetail).filter(ReportOrganizationDetail.organization_id == to_dict(item_delete2)['id']).all()
-    print ("_________-",item_delete3,to_dict(item_delete2)['id'])
-    for _ in item_delete3:
-        db.session.delete(_)
-        db.session.commit()
+    # item_delete3 = db.session.query(ReportOrganizationDetail).filter(ReportOrganizationDetail.organization_id == to_dict(item_delete2)['id']).all()
+    # print ("_________-",item_delete3,to_dict(item_delete2)['id'])
+    # for _ in item_delete3:
+    db.session.delete(item_delete2)
+    db.session.commit()
 
 
     data = request.json
